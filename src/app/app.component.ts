6ba656b88b0ece6b35f56853f2322b81b330af45
Sakshi Patel <w0860745@myscc.ca>
2024-09-23 13:06:51 -0400

    Add things in student-list.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,17 +15,4 @@ import {JsonPipe, NgForOf} from "@angular/common";
 export class AppComponent {
   title = 'User Generation';
 
-  user1: User = {id: 1, firstName: "Diana", lastName: "Nano", department: "Programming", isAdmin: false};
-  user2: User = {id: 2, firstName: "Matt", lastName: "Haug", department: "Web Dev", isAdmin: true};
-  //Can declare values either way
-  userList: User[] = //any[] would have worked as well
-    [this.user1, this.user2,
-      {id: 3, firstName: "John", lastName: "Doe", department: "Programming", isAdmin: false},
-      {id: 4, firstName: "Jane", lastName: "Doe", department: "Programming", isAdmin: true}
-    ]
-  //Function that gets called from our onclick. Takes in an
-  //argument ofa variable called user, which is type User and returns void
-  toggleAdminStatus(user: User): void {
-    user.isAdmin = !user.isAdmin;
-  }
 }
