085ffef205162b1fa708d662fdc83c421c873aea
Sakshi Patel <w0860745@myscc.ca>
2024-09-23 13:16:05 -0400

    All changes done

--- src/app/student-detail/student-detail.component.ts
+++ src/app/student-detail/student-detail.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {NgIf} from "@angular/common";
+import {User} from "../Shared/Models/user";
 
 @Component({
   selector: 'app-student-detail',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './student-detail.component.html',
   styleUrl: './student-detail.component.css'
 })
 export class StudentDetailComponent {
 
+  //Input marks the property as an input property
+//that can receive data from a parent
+  //? says it is optional
+  @Input() student?: User;
 }
--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -13,17 +13,19 @@ import {NgForOf} from "@angular/common";
 export class StudentListComponent {
 
 
-  user1: User = {id: 1, firstName: "Diana", lastName: "Nano", department: "Programming", isAdmin: false};
-  user2: User = {id: 2, firstName: "Matt", lastName: "Haug", department: "Web Dev", isAdmin: true};
-  //Can declare values either way
-  userList: User[] = //any[] would have worked as well
-    [this.user1, this.user2,
-      {id: 3, firstName: "John", lastName: "Doe", department: "Programming", isAdmin: false},
-      {id: 4, firstName: "Jane", lastName: "Doe", department: "Programming", isAdmin: true}
-    ]
-  //Function that gets called from our onclick. Takes in an
-  //argument ofa variable called user, which is type User and returns void
-  toggleAdminStatus(user: User): void {
-    user.isAdmin = !user.isAdmin;
+  //Placeholder values for the table
+  displayedColumns:string[]= ['id', 'firstName', 'lastName', 'department', 'isAdmin'];
+  userList: User[] = [ //Copied from app.component.ts
+    {id: 1, firstName: "Matt", lastName: "Haug", department: "Programming", isAdmin: false},
+    {id: 2, firstName: "Darren", lastName: "Takakki", department: "Web Dev", isAdmin: true},
+    {id: 3, firstName: "John", lastName: "Doe", department: "Programming", isAdmin: false},
+    {id: 4, firstName: "Jane", lastName: "Doe", department: "Programming", isAdmin:true}
+  ];
+
+  //Catch the onclick event from the html
+  selectedStudent?: User;
+  //function to set which student to display
+  selectStudent(student: User): void {
+    this.selectedStudent = student;
   }
 }
