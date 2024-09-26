8c6aee7fc874b2f99a5d5f07c8640fef75c1dc60
Sakshi Patel <w0860745@myscc.ca>
2024-09-26 19:00:44 -0400

    Week 4 in class lab 4 completed

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -8,7 +8,7 @@ import {Observable, of} from "rxjs";
 })
 export class StudentService {
 
-  //i needa local copy of the user list now
+  //i need a local copy of the user list now
 
   private students:User[] = userList;
 
@@ -16,8 +16,36 @@ export class StudentService {
 
   getStudents():Observable<User[]>{
     return of(userList); //this is returning an observable as it comes in
-
     //At the end of today we will add at CRUD operations for the rest of our service
+  }
 
+  //Adding basic CRUD methods
+  //Create : add user
+  addStudent(newStudent:User) : Observable<User[]>{
+    this.students.push(newStudent)
+    return of (this.students);
+  }
+
+  //Update an existing user
+  updateStudent(updatedStudent:User) : Observable<User[]>{
+    const index = this.students.findIndex(user=>user.id === updatedStudent.id)
+    if(index !== -1){
+      this.students[index] = updatedStudent;
+    }
+
+    return of (this.students);
+  }
+
+
+  //delete remove an user by id
+  deleteStudent(studentId:number):Observable<User[]>{
+    this.students = this.students.filter(user=>user.id !== studentId);
+    return of (this.students);
+  }
+
+  //get student by id
+  getStudentById(studentId: number): Observable<User | undefined> {
+    const student = this.students.find(user => user.id === studentId);
+    return of(student);
   }
 }
--- src/app/student-detail/student-detail.component.ts
+++ src/app/student-detail/student-detail.component.ts
@@ -11,8 +11,19 @@ import {User} from "../Shared/Models/user";
 })
 export class StudentDetailComponent {
 
-  //Input marks the property as an input property
-//that can receive data from a parent
-  //? says it is optional
-  @Input() student?: User;
+  //Input marks the property as an input property that can receive data from a parent
+  @Input() student?:User;
+  /*
+  @Input is known as a decorator in Angular. It is a special kind of declaration that can be attached to a
+  class declaration, method, accessor, property, or parameter. Decorators use the form @expression,
+ where expression must evaluate to a function that will be called at runtime with information about the
+ decorated declaration.
+
+ In particular, this let's Angular know that the variable student is an input property that can be passed
+  from the parent component. This is how we can pass data from the parent component to the child component.
+
+  The ? indicates it is optional, and the User keyword is our interface that we created in the Models folder.
+   */
+
+
 }
